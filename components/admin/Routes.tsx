70778941f319257e837219bc6e12bbacebc5893d
torres-leo <leojts24@gmail.com>
2024-04-11 21:37:29 -0600

    Add test comment in Routes.tsx

--- components/admin/Routes.tsx
+++ components/admin/Routes.tsx
@@ -19,6 +19,8 @@ const Route = ({ link }: RouteProps) => {
 	const pathName = usePathname();
 	const isActive = pathName === link.url;
 
+	// This is for test
+
 	const renderIcon = (icon: string) => {
 		if (icon === 'order') {
 			return (
