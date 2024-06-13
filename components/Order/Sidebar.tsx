9093a1fbcafdca0e78685c54c9ec790f2c237735
torres-leo <leojts24@gmail.com>
2024-06-12 22:03:38 -0600

    add button to redirect admin page

--- components/Order/Sidebar.tsx
+++ components/Order/Sidebar.tsx
@@ -6,6 +6,7 @@ import { getCategories } from '@/src/utils/getCategories';
 
 import CategoryItem from '../ui/CategoryItem';
 import Logo from '../ui/Logo';
+import Link from 'next/link';
 
 export default function OrderSidebar() {
 	const [categories, setCategories] = useState<Category[]>([]);
@@ -28,10 +29,19 @@ export default function OrderSidebar() {
 	});
 
 	return (
-		<aside className='aside'>
-			<Logo redirect='order/coffee' />
-			<p className='mb-6 text-center font-semibold text-xl select-none'>Categories</p>
-			<nav>{renderCategories}</nav>
+		<aside className='aside flex flex-col justify-between'>
+			<div>
+				<Logo redirect='order/coffee' />
+				<p className='mb-6 text-center font-semibold text-xl select-none'>Categories</p>
+				<nav>{renderCategories}</nav>
+			</div>
+
+			<Link
+				href='/admin/orders'
+				target='_blank'
+				className='capitalize text-amber-400 text-center w-fit mx-auto hover:text-white transition-colors px-4 py-1 border border-amber-400'>
+				Go to admin page
+			</Link>
 		</aside>
 	);
 }
